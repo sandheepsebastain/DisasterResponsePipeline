839c5a520a9ca46ee522fb90cdedb5c9fdd397e9
sandheepsebastian <sandheep.sebastian@woodmac.com>
2022-09-29 13:20:00 -0400

    Finished process_data.py

--- data/process_data.py
+++ data/process_data.py
@@ -1,15 +1,54 @@
+# import libraries
 import sys
-
+import pandas as pd
+from sqlalchemy import create_engine
 
 def load_data(messages_filepath, categories_filepath):
-    pass
-
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+    df = messages.merge(categories,how="inner",on=['id'])
+    return df
 
 def clean_data(df):
-    pass
+    categories=df[['id','categories']].copy()
+    categories.set_index('id',inplace=True)
+    # create a dataframe of the 36 individual category columns
+    categories=categories['categories'].str.split(";",expand=True)
+    
+    # select the first row of the categories dataframe
+    row = list(categories.iloc[0])
+    
+    # use this row to extract a list of new column names for categories.
+    # one way is to apply a lambda function that takes everything 
+    # up to the second to last character of each string with slicing
+    category_colnames =[ x[0:len(x)-2] for x in row]
+    categories.columns = category_colnames
+    
+    #Convert category values to just numbers 0 or 1
+    for column in categories:
+        # set each value to be the last character of the string
+        categories[column] = categories[column].apply(lambda x:x[-1:])
+    
+        # convert column from string to numeric
+        categories[column] = categories[column].astype(int)
+        
+    #Merging the categories dataframe back to the original dataframe
+    del df['categories']
+    
+    # concatenate the original dataframe with the new `categories` dataframe
+    categories.reset_index(inplace=True)
+    df = df.merge(categories,how="inner",on=['id'])
 
+    # dropping duplicate rows in the dataset
+    df.drop_duplicates(subset=['id'], keep="first", inplace=True)
+    
+    #return cleaned dataframe
+    return df
+    
 
 def save_data(df, database_filename):
+    engine = create_engine('sqlite:///'+database_filename)
+    df.to_sql('Messages', engine, index=False)
     pass  
 
 
